f177881232a249fb6fe862a0c954efd1c7f2f628
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-16 22:15:38 +0200

    wip-tetris-project

---
+++ app.js
@@ -0,0 +1,5 @@
+/* The DOM loaded event fires when the initial html document completed loaded and passed.
+The target of our event is the document we are referring */ 
+document.addEventListener('DOMContentLoaded',() => {
+
+})
